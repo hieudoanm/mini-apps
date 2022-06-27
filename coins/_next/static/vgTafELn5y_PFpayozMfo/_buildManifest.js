a52bef668ff82ec7d124f9f4e731240da3f5df4e
hieudoanm <hieumdoan@gmail.com>
2022-06-27 08:56:24 +0000

    Deploying to gh-pages from @ hieudoanm/mini-apps@c7d9e04545b769d0b177ddad0546caeb34f3f286 🚀

--- coins/_next/static/vgTafELn5y_PFpayozMfo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/977-6473586010f7313b.js","static/chunks/pages/index-a77653a7e70cebad.js"],"/_error":["static/chunks/pages/_error-9c4845c5224dedaa.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
