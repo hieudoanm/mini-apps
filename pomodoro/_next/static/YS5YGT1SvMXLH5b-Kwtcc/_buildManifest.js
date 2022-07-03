6e9494bbdd6c553a934507cf73d9e929fe2d38eb
hieudoanm <hieumdoan@gmail.com>
2022-07-03 08:49:53 +0000

    Deploying to gh-pages from @ hieudoanm/mini-apps@5fa358a384d4a1759f674477cbe04495152663e6 🚀

---
+++ pomodoro/_next/static/YS5YGT1SvMXLH5b-Kwtcc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/188-f05842979dcb2756.js","static/chunks/pages/index-89d954671a6af95c.js"],"/_error":["static/chunks/pages/_error-e755336071b61fcc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
